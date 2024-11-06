1ae1baa77a4b95abedff1b9b5084f2789537ec32
Sahid-Morales-5500 <sahidm2023@gmail.com>
2024-11-06 17:37:42 -0500

    CORRECICON

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 
 set -o errexit
 
-pipenv install -r Pipfile
+# Si estás usando un Pipfile, instala así:
+pipenv install  # Esto instalará las dependencias del Pipfile
 
-python manage.py collectstatic --noinput
-python manage.py migrate
+# O, si prefieres `requirements.txt`, usa:
+# pip install -r requirements.txt
+
+# Ejecuta las tareas de Django
+pipenv run python manage.py collectstatic --noinput
+pipenv run python manage.py migrate
